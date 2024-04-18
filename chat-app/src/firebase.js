9b8df0594cd78cd06b857a5a81963bd021353dc4
Pratik Rajput <pratikrajput1199@gmail.com>
2024-04-18 23:57:27 +0530

    commit

--- chat-app/src/firebase.js
+++ chat-app/src/firebase.js
@@ -5,23 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import { API_KEY, AppId, PROJECT_ID, SENDERID } from "./firebaseData";
 
-// const firebaseConfig = {
-//   apiKey: API_KEY,
-//   authDomain: "mychatapp-79307.firebaseapp.com",
-//   projectId: PROJECT_ID,
-//   storageBucket: "mychatapp-79307.appspot.com",
-//   messagingSenderId: SENDERID,
-//   appId: AppId,
-//   measurementId: "G-D8XN0RQCCW"
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA7w4BpH43qickUrjd19EeIcQj2EI1O8es",
+  apiKey: API_KEY,
   authDomain: "mychatapp-79307.firebaseapp.com",
-  projectId: "mychatapp-79307",
+  projectId: PROJECT_ID,
   storageBucket: "mychatapp-79307.appspot.com",
-  messagingSenderId: "940971514082",
-  appId: "1:940971514082:web:af2d6257910a3940b4a6bb",
+  messagingSenderId: SENDERID,
+  appId: AppId,
   measurementId: "G-D8XN0RQCCW"
 };
 
